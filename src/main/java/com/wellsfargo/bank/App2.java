a2f49f8a62fc469b878fd6d9fabd9764bce15023
srinath <srinath.purushothan@gmail.com>
2017-04-25 21:37:59 +0530

    adding maven

---
+++ src/main/java/com/wellsfargo/bank/App2.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Its ok World!" );
+    }
+}
